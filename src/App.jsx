ab007d91af0ca91139d114da61ebe444043daf1a
foojoowee <foo.joowee@gmail.com>
2023-07-03 16:30:11 +0800

    add: First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Piano from './components/Piano'
+import Key from './components/Key'
+
+function App() {
+  return (
+    <>
+      <Piano/>
+    </>
+  )
+}
+
+export default App
